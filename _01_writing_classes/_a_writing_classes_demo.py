95d1a5cbccc0ebe534591921486be7f9d7c74752
league <gmjangwoo0827@gmail.com>
2024-02-23 18:30:03 -0800

    almost finished duck

--- _01_writing_classes/_a_writing_classes_demo.py
+++ _01_writing_classes/_a_writing_classes_demo.py
@@ -30,9 +30,10 @@ class Duck(Animal):
     # This is a constructor! It's called when an object of this class
     # is created. The 'self' variable is always the first input parameter
     # and is used to create instance variables and call methods.
-    def __init__(self, name):
-
+    def __init__(self, name, age, fact):
+        self.duckfact = ''.join(fact)
         Duck.num_ducks_created = Duck.num_ducks_created + 1
+        self.duckage = int(age)
 
         # Instance variables start with 'self.' and can only be created in
         # the constructor
@@ -44,27 +45,48 @@ class Duck(Animal):
 
         # Calling a method also begins with '.self'
         self.initialize()
+        self.fact()
+        self.age()
 
     # This is a method!
     def initialize(self):
         # The instance variable can be used throughout the class
         print(self.name + ' the Duck was created!')
+    def fact(self):
+        print(f"One fact about {self.name}:")
+        print(self.duckfact)
+    def age(self):
+        print(f"My age is: {self.duckage}")
 
 
 if __name__ == '__main__':
     # TODO 1) Create an object of the Duck class, for example
     #  kenny = Duck('Kenny')
+    while True:
+        print("Create a Duck!: ")
+        user_inp = input()
+        user_inp = user_inp.replace(".", "")
+        create_str = []
+        create_str = user_inp.split()
+        if create_str[0] == "duck":
+            create_str[2] = int(create_str[2])
+            num = len(create_str) - 3
+            fact = ""
+            for i in range(num):
+                fact = fact + " " + create_str[i+3]
+            # TODO 2) Add 2 more input variables into the Duck constructor
+            duck = Duck(create_str[1], create_str[2], fact= fact)
+            # TODO 3) Create 2 instance variables in the Duck class
 
-    # TODO 2) Add 2 more input variables into the Duck constructor
+            # TODO 4) Create 2 more methods in the Duck class,
+            #  for example quack or waddle
 
-    # TODO 3) Create 2 instance variables in the Duck class
+            # TODO 5) Create 2 more Ducks with different names and call the methods
+            #  created in the previous step
 
-    # TODO 4) Create 2 more methods in the Duck class,
-    #  for example quack or waddle
-
-    # TODO 5) Create 2 more Ducks with different names and call the methods
-    #  created in the previous step
-
-    if Duck.num_ducks_created < 3:
-        print('There are only ' + str(Duck.num_ducks_created) + ' Ducks.')
-        print('Create more Ducks!!!')
+            if Duck.num_ducks_created < 3 and Duck.num_ducks_created > 1:
+                print('There are only ' + str(Duck.num_ducks_created) + ' Ducks.')
+                print('Create more Ducks!!!')
+            elif Duck.num_ducks_created == 1:
+                print('There is only 1 Duck.')
+                print('Create more Ducks!!!')
