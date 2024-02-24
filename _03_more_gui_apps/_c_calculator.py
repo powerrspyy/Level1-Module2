5c9877750e34e408d98bca5795eeb1f79ff85e75
powerrspyy <147744817+powerrspyy@users.noreply.github.com>
2024-02-23 18:02:26 -0800

    Initial commit

---
+++ _03_more_gui_apps/_c_calculator.py
@@ -0,0 +1,8 @@
+"""
+Create a simple calculator app
+"""
+import tkinter as tk
+
+# TODO Make a calculator app like the one shown in the calculator.png image
+#  located in this folder.
+#  HINT: you'll need: 2 text fields, 4 buttons, and 1 label
