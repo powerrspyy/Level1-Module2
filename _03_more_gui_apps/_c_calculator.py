56d1ba6914e268bd34ccf37ec53a7e4e46d11717
Hailin Day <haililnday@gmail.com>
2024-03-15 17:34:42 -0700

    finished

--- _03_more_gui_apps/_c_calculator.py
+++ _03_more_gui_apps/_c_calculator.py
@@ -15,11 +15,17 @@ class ButtonsAndTextFields(tk.Tk):
         #   (without the parenthesis) is used it will get called when the
         #   button is pressed.
         self.button = tk.Button(self, text='Add', fg='black',
-                                font=('courier new', 16, 'bold'), command=self.on_button_press(type = 1))
-        self.button.place(relx=0.1, rely=0.3, relwidth=0.5, relheight=0.2)
-        self.button = tk.Button(self, text='Subtract', fg='black',
-                                font=('courier new', 16, 'bold'), command=self.on_button_press(type = 2))
-        self.button.place(relx=0.1, rely=0.3, relwidth=0.5, relheight=0.2)
+                                font=('courier new', 11, 'bold'), command=self.on_button_press)
+        self.button.place(relx=0.05, rely=0.3, relwidth=0.2, relheight=0.1)
+        self.button2 = tk.Button(self, text='Subtract', fg='black',
+                                font=('courier new', 11, 'bold'), command=self.on_button_press2)
+        self.button2.place(relx=0.25, rely=0.3, relwidth=0.2, relheight=0.1)
+        self.button3 = tk.Button(self, text='Multiply', fg='black',
+                                font=('courier new', 11, 'bold'), command=self.on_button_press3)
+        self.button3.place(relx=0.45, rely=0.3, relwidth=0.2, relheight=0.1)
+        self.button4 = tk.Button(self, text='Divide', fg='black',
+                                font=('courier new', 11, 'bold'), command=self.on_button_press4)
+        self.button4.place(relx=0.65, rely=0.3, relwidth=0.2, relheight=0.1)
 
         # Add a text field that can take input from the user
         self.text_field = tk.Entry(self)
@@ -47,19 +53,28 @@ class ButtonsAndTextFields(tk.Tk):
         # and leaves the label object
         self.label = tk.Label(self, bg = 'white', font=('Rockwell', 20, 'normal', 'underline'), fg = 'red')
         self.label.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.2)
-    def on_button_press(self, type: int):
+    def on_button_press(self):
         # Get the text from the text field and store into a variable
         f1 = self.text_field.get()
         f2 = self.text_field2.get()
-        if type == 1:
         # Update the text in the label
-            self.label.configure(text=int(f1) + int(f2))
+        self.label.configure(text=int(f1) + int(f2))
+    def on_button_press2(self):
+        # Get the text from the text field and store into a variable
+        f1 = self.text_field.get()
+        f2 = self.text_field2.get()
 
-        elif type == 2:
-            self.label.configure(text=int(f1) - int(f2))
-        elif type == 3:
-            self.label.configure(text=int(f1) * int(f2))
-
+        self.label.configure(text=int(f1) - int(f2))
+    def on_button_press3(self):
+        # Get the text from the text field and store into a variable
+        f1 = self.text_field.get()
+        f2 = self.text_field2.get()
+        self.label.configure(text=int(f1) * int(f2))
+    def on_button_press4(self, type: int = 4):
+        # Get the text from the text field and store into a variable
+        f1 = self.text_field.get()
+        f2 = self.text_field2.get()
+        self.label.configure(text =int(f1)/int(f2))
 
     def on_text_entry(self, event):
         print(repr(event))
@@ -71,9 +86,7 @@ class ButtonsAndTextFields(tk.Tk):
         print(repr(event))
         print('  num ...: ' + str(event.num) + ' (button number)')
 
-    def on_button_press2(self, event):
-        print(repr(event))
-        print('  num ...: ' + str(event.num) + ' (button number)')
+
 
 if __name__ == '__main__':
     app = ButtonsAndTextFields()
